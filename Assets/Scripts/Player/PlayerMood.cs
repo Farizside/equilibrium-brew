56a8a7412d5d1f65b2559516f4483f0aee6a92ac
FRZ <muhammadfarizh31@gmail.com>
2024-06-02 01:12:12 +0700

    feat: Mood System

--- Assets/Scripts/Player/PlayerMood.cs
+++ Assets/Scripts/Player/PlayerMood.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,27 +6,100 @@ using UnityEngine.UI;
 
 public class PlayerMood : MonoBehaviour
 {
+    [SerializeField] private Mood _mood;
+    
     [Range(-1, 1)]
     [SerializeField] private float _moodLevel;
 
     [SerializeField] private float _moodSwingLevel;
 
+    [SerializeField] private float _manicThreshold;
+
+    [SerializeField] private float _depressThreshold;
+
     [SerializeField] private Slider _moodSlider;
+
+    private Action _onMoodValueChanged;
+    private Action _onMoodChanged;
+
+    private Mood Mood
+    {
+        get => _mood;
+        set
+        {
+            _mood = value;
+            _onMoodChanged?.Invoke();
+        }
+    }
+
+    private float MoodLevel
+    {
+        get => _moodLevel;
+        set
+        {
+            _moodLevel = value;
+            _onMoodValueChanged?.Invoke();
+            if (MoodLevel >= _manicThreshold)
+            {
+                Mood = Mood.Manic;
+            }else if (MoodLevel <= _depressThreshold)
+            {
+                Mood = Mood.Depress;
+            }
+            else
+            {
+                Mood = Mood.Stable;
+            }
+        }
+    }
+
+    public void OnEnable()
+    {
+        _onMoodValueChanged += HandleUI;
+        _onMoodChanged += HandleMoodChanged;
+    }
+
+    private void OnDisable()
+    {
+        _onMoodValueChanged -= HandleUI;
+        _onMoodChanged -= HandleMoodChanged;
+    }
 
     public void DepressMood()
     {
-        _moodLevel -= _moodSwingLevel;
-        HandleUI();
+        MoodLevel -= _moodSwingLevel;
     }
     
     public void ManicMood()
     {
-        _moodLevel += _moodSwingLevel;
-        HandleUI();
+        MoodLevel += _moodSwingLevel;
+    }
+
+    public void StabilizeMood(float value)
+    {
+        if (MoodLevel < 0)
+        {
+            MoodLevel += value;
+        }else if (MoodLevel > 0)
+        {
+            MoodLevel -= value;
+        }
     }
 
     public void HandleUI()
     {
         _moodSlider.value = _moodLevel;
     }
+
+    public void HandleMoodChanged()
+    {
+        Debug.Log(_mood);
+    }
+}
+
+public enum Mood
+{
+    Stable,
+    Manic,
+    Depress
 }
