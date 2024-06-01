00692c45af438d755e4e8ddaabbd60837f9f856f
Apriana <aprianacahya04@gmail.com>
2024-06-02 03:16:53 +0700

    animate char in map

--- Assets/Scripts/Map/BuildingController.cs
+++ Assets/Scripts/Map/BuildingController.cs
@@ -8,12 +8,14 @@ public class BuildingController : MonoBehaviour{
     public GameObject pointer;
     public int idxBuilding;
     public string buildingScene;
-    private MapSystem _MapSystem;
+    public Animator mAnimatorChar;
+    private MapSystem _mapSystem;
+    
     // Start is called before the first frame update
     void Start()
     {
-        _MapSystem = MapSystem.Instance;
-        pointer.SetActive(_MapSystem.currentBuilding == idxBuilding);
+        _mapSystem = MapSystem.Instance;
+        pointer.SetActive(_mapSystem.currentBuilding == idxBuilding);
     }
 
 
@@ -30,7 +32,85 @@ public class BuildingController : MonoBehaviour{
 
     private void OnMouseDown()
     {
-        _MapSystem.currentBuilding = idxBuilding;
+        MoveCharAnimation(_mapSystem.currentBuilding,idxBuilding);
+        _mapSystem.currentBuilding = idxBuilding;
+        StartCoroutine(ChangeSceneFinisAnim());
+        // UnityEngine.SceneManagement.SceneManager.LoadScene(buildingScene);
+    }
+
+    IEnumerator ChangeSceneFinisAnim()
+    {
+        while (mAnimatorChar.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
+        {
+            yield return null;
+        }
         UnityEngine.SceneManagement.SceneManager.LoadScene(buildingScene);
+    }
+    
+    private void MoveCharAnimation(int currentBuilding, int gotoBuilding)
+    {
+        switch(currentBuilding) 
+        {
+            case 1:
+                switch (gotoBuilding)
+                {
+                    case 2:
+                        mAnimatorChar.SetTrigger("B1toB2");
+                        break;
+                    case 3:
+                        mAnimatorChar.SetTrigger("B1toB3");
+                        break;
+                    case 4:
+                        mAnimatorChar.SetTrigger("B1toB4");
+                        break;
+                }
+                break;
+            case 2:
+                switch (gotoBuilding)
+                {
+                    case 1:
+                        mAnimatorChar.SetTrigger("B2toB1");
+                        break;
+                    case 3:
+                        mAnimatorChar.SetTrigger("B2toB3");
+                        break;
+                    case 4:
+                        mAnimatorChar.SetTrigger("B2toB4");
+                        break;
+                }
+                break;
+            
+            case 3:
+                switch (gotoBuilding)
+                {
+                    case 1:
+                        mAnimatorChar.SetTrigger("B3toB1");
+                        break;
+                    case 2:
+                        mAnimatorChar.SetTrigger("B3toB2");
+                        break;
+                    case 4:
+                        mAnimatorChar.SetTrigger("B3toB4");
+                        break;
+                }
+                break;
+            case 4:
+                switch (gotoBuilding)
+                {
+                    case 1:
+                        mAnimatorChar.SetTrigger("B4toB1");
+                        break;
+                    case 2:
+                        mAnimatorChar.SetTrigger("B4toB2");
+                        break;
+                    case 3:
+                        mAnimatorChar.SetTrigger("B4toB3");
+                        break;
+                }
+                break;
+            default:
+                Debug.Log(("Invalid"));
+                break;
+        }
     }
 }
