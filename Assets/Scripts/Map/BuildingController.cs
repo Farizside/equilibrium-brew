1af7dca54150bfe3b1c8caeb8dc3fa9980813956
aprnna <aprianacahya04@gmail.com>
2024-06-10 17:38:01 +0700

    update map

--- Assets/Scripts/Map/BuildingController.cs
+++ Assets/Scripts/Map/BuildingController.cs
@@ -9,19 +9,19 @@ public class BuildingController : MonoBehaviour{
     public GameObject pointer;
     public int idxBuilding;
     public string buildingScene;
-    private MapSystem _MapSystem;
-
+    private MapSystem _mapSystem;
+    public Animator mAnimatorChar;
     [SerializeField] private string _buildingName;
 
     private StoryManager _storyManager;
     // Start is called before the first frame update
     void Start()
     {
-        _MapSystem = MapSystem.Instance;
+        _mapSystem = MapSystem.Instance;
         _storyManager = StoryManager.Instance;
         
-        // pointer.SetActive(_MapSystem.currentBuilding == idxBuilding);
-        pointer.SetActive(_MapSystem.currentBuildingName == _buildingName);
+        pointer.SetActive(_mapSystem.currentBuilding == idxBuilding);
+        // pointer.SetActive(_mapSystem.currentBuildingName == _buildingName);
     }
 
 
@@ -38,8 +38,9 @@ public class BuildingController : MonoBehaviour{
 
     private void OnMouseDown()
     {
-        _MapSystem.currentBuilding = idxBuilding;
-        _MapSystem.currentBuildingName = _buildingName;
+        MoveCharAnimation(_mapSystem.currentBuilding,idxBuilding);
+        _mapSystem.currentBuilding = idxBuilding;
+        _mapSystem.currentBuildingName = _buildingName;
 
         if (_storyManager.IdxStory == 1)
         {
@@ -48,10 +49,18 @@ public class BuildingController : MonoBehaviour{
         }
         else
         {
-            SceneManager.LoadScene(_buildingName);
+            StartCoroutine(ChangeSceneFinisAnim());
         }
     }
-    
+    IEnumerator ChangeSceneFinisAnim()
+    {
+        while (mAnimatorChar.GetCurrentAnimatorStateInfo(0).normalizedTime < 1f)
+        {
+            yield return new WaitForSeconds(mAnimatorChar.runtimeAnimatorController.animationClips[0].length);
+        }
+        SceneManager.LoadScene(buildingScene);
+    }
+
     private void MoveCharAnimation(int currentBuilding, int gotoBuilding)
     {
         switch(currentBuilding) 
